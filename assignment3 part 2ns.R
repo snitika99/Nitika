8f0f240f63a7728a35f106337ace7566942be4fb
Nitika sharma <63331828+snitika99@users.noreply.github.com>
2020-06-05 12:46:11 +1000

    Update assignment3 part 2ns.R
    from 49th lin

--- assignment3 part 2ns.R
+++ assignment3 part 2ns.R
@@ -45,4 +45,27 @@ myseqs <-c(myseqs,E.COLI)
 seqinr:: write.fasta (myseq,names = names(myseqs), file.out="myseqs.fa")
 str(myseqs)  
 #names of sequence of first top last hit
-
+new one 
+#question1
+#E.COLI GENE SEQUENCE
+download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",destfile = "Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz")
+#DECOMPRESS
+R.utils::gunzip("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",overwrite=TRUE)
+#Blast database
+makeblastdb("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa",dbtype = "nucl","-parse_seqids")
+#Sequences present
+str("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz")
+seqinr::getLength("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz")
+#question2
+#download and reading sequence length festa file
+E.COLI <-seqinr::read.fasta("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/sample.fa")
+str(E.COLI)
+#read the festa file
+E.COLI<-seqinr::read.fasta("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa")
+#selecting gene of interest
+geneofinterest <- E.COLI[[10]]
+str(geneofinterest)
+#determining the length
+seqinr:: getLength(geneofinterest)
+#proportion of GC
+seqinr::GC(geneofinterest)
